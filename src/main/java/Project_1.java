77d9f44be6a2a1380b642ed06603100f38b22663
dmitriyart1389 <52502298+dmitriyart1389@users.noreply.github.com>
2021-08-04 20:28:17 +0300

    Update Project_1.java
    Функции

--- src/main/java/Project_1.java
+++ src/main/java/Project_1.java
@@ -1,9 +1,12 @@
 public class Project_1 {
     public static void main(String[] args) {
 
-        int l = 8;
-        int s = l * l;
-        System.out.println("Площадь квадрата со стороной " + l + " = " + s);
+        hello("world");
+    }
+
+    public static void hello(String somebody) {
+//        String somebody = "world";
+        System.out.println("Hello, " + somebody);
     }
 
 }
