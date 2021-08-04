dfeb39353c9df8d115ea74c9a54a93d3b2174e01
dmitriyart1389 <52502298+dmitriyart1389@users.noreply.github.com>
2021-08-04 20:10:08 +0300

    Переменные

--- src/main/java/Project_1.java
+++ src/main/java/Project_1.java
@@ -1,6 +1,9 @@
 public class Project_1 {
     public static void main(String[] args) {
 
+        int l = 8;
+        int s = l * l;
+        System.out.println("Площадь квадрата со стороной " + l + " = " + s);
     }
 
 }
